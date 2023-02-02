4459fe667d6f1cc1143a4c37c84abf7e58e19bb7
luluco92 <117111188+luluco92@users.noreply.github.com>
2023-02-02 13:58:24 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const db = require('./config/connection');
+// const routes = require('./routes');
+
+const { Book } = require('./models');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes);
+
+app.get('/all-books', (req, res) => {
+
+ Book.find({}, (err, result) => {
+    if (err) {
+      res.status(500).send({ message: 'Internal Server Error' });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+});

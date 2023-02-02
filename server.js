70725af8d29802e9debb93e11fb8da7e16829fd6
luluco92 <117111188+luluco92@users.noreply.github.com>
2023-02-02 14:05:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 // const routes = require('./routes');
 
-const { Book } = require('./models');
+const { User, Post, Response, Friend } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
